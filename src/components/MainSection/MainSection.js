646928f592ae391123a83bdd2c8161a462edcdd1
Bullishbean <bullish354@gmail.com>
2024-12-15 16:43:00 +0000

    Your commit message

--- src/components/MainSection/MainSection.js
+++ src/components/MainSection/MainSection.js
@@ -55,8 +55,8 @@ const MainSection = () => {
   const handleBuy = async () => {
     setLoading(true);
     try {
-      if (amount < 0.03) {
-        toast.error("Minimum buy is 0.03 BNB");
+      if (amount < 0.014) {
+        toast.error("Minimum buy is 0.014 BNB");
         setLoading(false);
         return;
       }
@@ -173,7 +173,7 @@ const MainSection = () => {
                     </li>
                     <li>
                       <p>Minimum Buy</p>
-                      <h6>0.03 BNB</h6>
+                      <h6>0.014 BNB</h6>
                     </li>
                   </ul>
                 </div>
