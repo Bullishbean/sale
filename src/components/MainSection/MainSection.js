7b573fcd7de77bd297bf7c4d43e3639c2fb891bc
Bullishbean <bullish354@gmail.com>
2026-04-22 13:22:11 +0000

    Update MainSection.js

--- src/components/MainSection/MainSection.js
+++ src/components/MainSection/MainSection.js
@@ -55,8 +55,8 @@ const MainSection = () => {
   const handleBuy = async () => {
     setLoading(true);
     try {
-      if (amount < 0.50) {
-        toast.error("Minimum buy is 0.50 BNB");
+      if (amount < 0.0065) {
+        toast.error("Minimum buy is 0.0065 BNB");
         setLoading(false);
         return;
       }
@@ -164,11 +164,11 @@ const MainSection = () => {
                     </li>
                     <li>
                       <p>Current Price</p>
-                      <h6>0.0007800 USDT</h6>
+                      <h6>0.0008800 USDT</h6>
                     </li>
                     <li>
                       <p>Minimum Buy</p>
-                      <h6>0.50 BNB</h6>
+                      <h6>0.065 BNB</h6>
                     </li>
                   </ul>
                 </div>
